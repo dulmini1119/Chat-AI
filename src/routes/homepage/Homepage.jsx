93c3fb5dc98b1520539eeccd9e9edb866ba5da8a
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2025-02-12 23:00:55 +0530

    Create HomePage

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -5,14 +5,22 @@ import { Link } from 'react-router-dom'
 const Homepage = () => {
   return (
     <div className='homepage'>
+      <img className='orbital' src='/orbital.avif'/>
       <div className='left'>
         <h1>ZC AI</h1>
         <h2>Supercharge your creativity and productivity</h2>
         <h3>Lorem ipsum dolor sit amet consectetur 
           adipisicing elit. </h3>
-          <button>Get Started</button>
+          <Link to='/dashboard'>Get Started</Link>
       </div>
-      <div className='right'></div>
+      <div className='right'>
+        <div className='imgContainer'>
+          <div className='bgContainer'>
+            <div className='bg'></div>
+          </div>
+          <img src='/bot.png' alt='bot' className='bot'/>
+        </div>
+      </div>
     </div>
   )
 }
