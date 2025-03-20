8a085448fd819abc1f41608aef62e36ba2ff27d8
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2025-03-20 13:25:01 +0530

    Create HomePage

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './homepage.css'
 import { Link } from 'react-router-dom'
+import { TypeAnimation } from 'react-type-animation'
 
 const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("Human1");
   return (
     <div className='homepage'>
       <img className='orbital' src='/orbital.avif'/>
@@ -19,6 +21,41 @@ const Homepage = () => {
             <div className='bg'></div>
           </div>
           <img src='/bot.png' alt='bot' className='bot'/>
+          <div className="chat">
+            <img src={typingStatus === 'Human1' ? "/human1.jpg" : typingStatus === "Human2" ? "/human2.png" : "/bot.png"}/>
+          <TypeAnimation
+      sequence={[
+        'Human1:  What is the full form of CPU?',
+        2000, () => {
+          setTypingStatus("bot");
+        },
+        'Bot : CPU standards for Central Processing Unit',
+        2000,() => {
+          setTypingStatus("Human2");
+        },
+        'Human2 : What does ROM stand for?',
+        2000,() => {
+          setTypingStatus("bot");
+        },
+        'Bot : ROM standards for Read-Only Memory',
+        2000,() => {
+          setTypingStatus("Human1");
+        },
+      ]}
+      wrapper="span"
+      repeat={Infinity}
+      cursor={true}
+      omitDeletionAnimation={true}
+    />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src='/logo.png' alt='logo'/>
+        <div className="links">
+          <Link to='/'>Terms of Services</Link>
+          <span>|</span>
+          <Link to='/'>Privacy Policy</Link>
         </div>
       </div>
     </div>
