19ab96bd05254269414fe3fc8cb516c4e5f02ccb
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2025-03-22 22:09:14 +0530

    Dashboard

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -3,7 +3,38 @@ import './dashboardPage.css'
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div className="dashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <img src="logo.png" alt="Logo" />
+          <h1>ZC AI</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src='/chat.png' alt=''/>
+            <span>Create a new Chat</span>
+          </div>
+
+          <div className="option">
+            <img src='/image.png' alt=''/>
+            <span>Analyze Images</span>
+          </div>
+
+          <div className="option">
+            <img src='/code.png' alt=''/>
+            <span>Help me with my Code</span>
+          </div>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" placeholder="Ask me Anything..."/>
+          <button>
+            <img src='/arrow.png' alt='arrow'/>
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
