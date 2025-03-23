37058873d818e39c908eccec7677116a9904b861
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2025-03-23 21:37:55 +0530

    ChatPage

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './ChatPage.css'
+import NewPrompt from '../../components/newPrompt/NewPrompt';
 
 const ChatPage = () => {
+  const endRef = useRef(null);
+
+  useEffect(()=> {
+    endRef.current.scrollIntoView({behavior : "smooth"});
+  },[])
+
+
   return (
-    <div>ChatPage</div>
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+
+          <NewPrompt/>
+
+          <div ref={endRef}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
