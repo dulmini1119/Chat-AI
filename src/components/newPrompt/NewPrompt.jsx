2767d2184dd9f14ba28d99a1525f4cfd9322524d
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2025-03-25 13:29:58 +0530

    NewPrompt

--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -1,10 +1,26 @@
 import './newPrompt.css'
+import React, { useEffect, useRef } from 'react'
 
 const NewPrompt = () => {
+  const endRef = useRef(null);
+
+  useEffect(()=> {
+    endRef.current.scrollIntoView({behavior : "smooth"});
+  },[])
   return (
-    <div className="newPrompt">
-        
-    </div>
+    <>
+    <div className="endChat" ref={endRef}></div>
+        <div className="newForm">
+          <label htmlFor='file'>
+            <img src='/attachment.png' alt='' />
+          </label>
+          <input type='file' id='file' multiple={false} hidden/>
+          <input type='text' placeholder='Ask Anything...'/>
+          <button>
+            <img src='/arrow.png' alt=''/>
+          </button>
+        </div>
+    </>
   )
 }
 
