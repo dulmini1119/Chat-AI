c3f82419ff5b259025cc02790b72aa9bf61d6aa8
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2025-03-28 23:25:31 +0530

    Initial Commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
 import express from "express";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/api/upload",(req,res) => {
+    res.send("It Works");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
